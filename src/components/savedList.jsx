189bc2d6880da887016fb5eb6043c99e8d8979c2
Débora Ribeiro <debora.marisa.ribeiro@gmail.com>
2021-11-04 17:10:08 +0100

    fix: overflow mobile

--- src/components/savedList.jsx
+++ src/components/savedList.jsx
@@ -75,6 +75,7 @@ const Filter = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    overflow-y: scroll;
     > div {
         width: 100%;
         text-align: center;
